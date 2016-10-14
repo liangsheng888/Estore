3c8b34b0867a6e8afdd0a53f18aa0c277c444f35
liangsheng888 <978188219@qq.com>
2016-10-14 16:16:30 +0800

    author:亮亮 desc:项目框架

--- app/src/main/java/com/estore/activity/ProOrderActivity.java
+++ app/src/main/java/com/estore/activity/ProOrderActivity.java
@@ -3,11 +3,15 @@ package com.estore.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 订单
+ */
+
 public class ProOrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pro_order);
+        setContentView(R.layout.activity_prod_order);
     }
 }
