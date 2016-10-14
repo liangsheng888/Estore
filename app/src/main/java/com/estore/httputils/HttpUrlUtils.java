47eefe98e55ade9eaf8cc4337335af0807ef1f56
Administrator <978188219@qq.com>
2016-10-14 11:49:27 +0800

    author:黄世兵 desction:二货界面

--- app/src/main/java/com/estore/httputils/HttpUrlUtils.java
+++ app/src/main/java/com/estore/httputils/HttpUrlUtils.java
@@ -4,6 +4,6 @@ package com.estore.httputils;
  * Created by Administrator on 2016/9/20.
  */
 public class HttpUrlUtils {
-    public  static  final  String HTTP_URL="http://10.40.5.38:8080/EStore/";
+    public  static  final  String HTTP_URL="http://10.40.5.34:8080/EStore/";
 
 }
