5ae43b9bb9ff40de9b31949958fe63d645de04b6
liangsheng888 <978188219@qq.com>
2016-10-17 21:42:45 +0800

    author:tfjin desc:选择地址listview框架

--- app/src/main/java/com/estore/activity/PaiMaiMain_bidding.java
+++ app/src/main/java/com/estore/activity/PaiMaiMain_bidding.java
@@ -2,12 +2,38 @@ package com.estore.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class PaiMaiMain_bidding extends AppCompatActivity {
+public class PaiMaiMain_bidding extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tv_bidding_addess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pai_mai_main_bidding);
+        initView();
+        initData();
+        initEven();
+    }
+    private void initView() {
+        tv_bidding_addess = ((TextView) findViewById(R.id.tv_bidding_addess));//选择地址
+    }
+    private void initEven() {
+        tv_bidding_addess.setOnClickListener(this);
+    }
+
+    private void initData() {
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_bidding_addess:
+                break;
+        }
     }
 }
