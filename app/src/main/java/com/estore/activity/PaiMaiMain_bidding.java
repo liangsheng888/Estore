a048b6a47c8b9dd4928158ebfa3e793238c3ca56
liangsheng888 <978188219@qq.com>
2016-10-17 18:56:23 +0800

    author:tfjin mess:修改上传拍卖页面,搭建竞拍协议界面

---
+++ app/src/main/java/com/estore/activity/PaiMaiMain_bidding.java
@@ -0,0 +1,13 @@
+package com.estore.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaiMaiMain_bidding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pai_mai_main_bidding);
+    }
+}
