5c12b079a8bbcc7d3d748a2a57dc584e92874ed0
liangsheng888 <978188219@qq.com>
2016-11-08 19:39:41 +0800

    author:tfjin desc:加返回

--- app/src/main/java/com/estore/activity/PaiMaiMain_bidding.java
+++ app/src/main/java/com/estore/activity/PaiMaiMain_bidding.java
@@ -1,12 +1,14 @@
 package com.estore.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.estore.R;
 import com.estore.pojo.Address;
@@ -20,11 +22,13 @@ public class PaiMaiMain_bidding extends AppCompatActivity implements View.OnClic
     private TextView tv_usertel;
     private TextView tv_address_info;
     Address address;
-    public  final static int SIGN=4; //传值回调标志位
+    public final static int SIGN = 4; //传值回调标志位
     private LinearLayout ll_auct_address;
     private Button btn_up_baozheng;
     AuctListActivityBean.Auct auct;
     Integer paiMaiChangCiFlag;
+    private TextView tv_info_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +38,8 @@ public class PaiMaiMain_bidding extends AppCompatActivity implements View.OnClic
         initEven();
         Intent intent = getIntent();
         auct = (AuctListActivityBean.Auct) intent.getSerializableExtra("auct");
-        paiMaiChangCiFlag=intent.getIntExtra("paiMaiChangCiFlag",0);
-        System.out.println("保证金页面的auct"+auct);
+        paiMaiChangCiFlag = intent.getIntExtra("paiMaiChangCiFlag", 0);
+        System.out.println("保证金页面的auct" + auct);
     }
 
     private void initView() {
@@ -45,12 +49,14 @@ public class PaiMaiMain_bidding extends AppCompatActivity implements View.OnClic
         tv_address_info = ((TextView) findViewById(R.id.tv_address_info));
         ll_auct_address = ((LinearLayout) findViewById(R.id.ll_auct_address));
         btn_up_baozheng = ((Button) findViewById(R.id.btn_up_baozheng));
+        tv_info_back = ((TextView) findViewById(R.id.tv_info_back));
 
     }
 
     private void initEven() {
         tv_bidding_addess.setOnClickListener(this);
         btn_up_baozheng.setOnClickListener(this);
+        tv_info_back.setOnClickListener(this);
     }
 
     private void initData() {
@@ -58,23 +64,28 @@ public class PaiMaiMain_bidding extends AppCompatActivity implements View.OnClic
     }
 
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 
-        address= (Address) data.getSerializableExtra("addressSign");
-        System.out.println("获得地址信息"+address);
+        address = (Address) data.getSerializableExtra("addressSign");
+        System.out.println("获得地址信息" + address);
         ll_auct_address.setVisibility(View.VISIBLE);
         System.out.println(address.cantactName);
-        tv_auct_shouhuo.setText(address.cantactName+"");
+        tv_auct_shouhuo.setText(address.cantactName + "");
         tv_usertel.setText(address.cantactPhone);
         tv_address_info.setText(address.detailed_address);
 
     }
 
 
-//    addressSign=intent.getIntExtra("addressSign",addressSign);
+    //    addressSign=intent.getIntExtra("addressSign",addressSign);
+    public void onBackPressed() {
+//        Log.d(TAG, "onBackPressed()");
+        super.onBackPressed();
+        Intent intent = new Intent(getApplicationContext(), PaimaiMain_infoActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     public void onClick(View view) {
@@ -84,17 +95,27 @@ public class PaiMaiMain_bidding extends AppCompatActivity implements View.OnClic
                 Bundle bundle = new Bundle();
                 bundle.putInt("addressSign", addressSign);
                 intent.putExtras(bundle);
-                startActivityForResult(intent,SIGN);
+                startActivityForResult(intent, SIGN);
+                break;
+            case R.id.tv_info_back:
+                intent = new Intent(getApplicationContext(), PaimaiMain_infoActivity.class);
+                startActivity(intent);
                 break;
             case R.id.btn_up_baozheng:
-                intent=new Intent(getApplicationContext(),PaiMAIbidding_bidActivity.class);
-                 bundle=new Bundle();
-                bundle.putSerializable("auct_bid",auct);
-                System.out.println("向加价界面传值"+auct);
-                bundle.putInt("paiMaiChangCiFlag",paiMaiChangCiFlag);
-                intent.putExtras(bundle);
-                startActivity(intent);
-
+                String userName=tv_auct_shouhuo.getText().toString().trim();
+                if (TextUtils.isEmpty(userName)){
+                    Toast.makeText(this, "请选择地址信息", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else {
+                    intent = new Intent(getApplicationContext(), PaiMAIbidding_bidActivity.class);
+                    bundle = new Bundle();
+                    bundle.putSerializable("auct_bid", auct);
+                    System.out.println("向加价界面传值" + auct);
+                    bundle.putInt("paiMaiChangCiFlag", paiMaiChangCiFlag);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }
                 break;
         }
     }
